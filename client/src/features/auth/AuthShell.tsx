6aeabf498735a9d0ee0e8bc606e6b5f63b0202e4
Silamsai <silamsai74@gmail.com>
2026-09-18 14:27:15 +0530

    login fix

--- client/src/features/auth/AuthShell.tsx
+++ client/src/features/auth/AuthShell.tsx
@@ -199,26 +199,25 @@ export default function AuthShell({ children, title, subtitle, showLegalFooter =
                             }}
                         >
                             Your business<br />online, locally
-                            {}
                             <svg
-                                width="40"
-                                height="30"
-                                viewBox="0 0 40 30"
+                                width="52"
+                                height="40"
+                                viewBox="0 0 52 40"
                                 fill="none"
-                                style={{ display: 'block', marginTop: 2, marginLeft: 'auto' }}
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden
+                                style={{ display: 'block', marginTop: 6, marginLeft: 'auto' }}
                             >
                                 <path
-                                    d="M4 2C12 14 24 20 36 16"
+                                    d="M4 6C12 8 22 20 34 28C36.5 29.5 39 30.5 42 31"
                                     stroke="#FF6A00"
-                                    strokeWidth="1.5"
+                                    strokeWidth="2.25"
                                     strokeLinecap="round"
+                                    fill="none"
                                 />
                                 <path
-                                    d="M32 12l4 4-5 2"
-                                    stroke="#FF6A00"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
+                                    d="M36.2 24.8L45.5 32.2L35.4 34.6L36.2 24.8Z"
+                                    fill="#FF6A00"
                                 />
                             </svg>
                         </div>
